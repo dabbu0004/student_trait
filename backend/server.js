bee9fcc6e1695c937afc503d412284cfb006426d
Himanshu <166318192+dabbu0004@users.noreply.github.com>
2026-01-22 15:01:50 +0530

    Refactor server.js to remove commented code

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ const app = express();
 ======================= */
 app.use(
   cors({
-    origin: "https://personality-trait-seven.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -147,3 +147,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
