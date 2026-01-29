199720c75311fbdd3bf95ffc887bd60c378367f4
Himanshu <166318192+dabbu0004@users.noreply.github.com>
2026-01-29 13:58:32 +0530

    Refactor TraitRange model to use grade and trait

--- backend/models/TraitRange.js
+++ backend/models/TraitRange.js
@@ -1,11 +1,28 @@
+// const mongoose = require('mongoose');
+
+// const traitRangeSchema = new mongoose.Schema({
+//   minGrade: Number,
+//   maxGrade: Number,
+//   trait: String
+// });
+
+// const TraitRange = mongoose.model('TraitRange', traitRangeSchema);
+
+
+// module.exports = TraitRange;
+
+
 const mongoose = require('mongoose');
 
 const traitRangeSchema = new mongoose.Schema({
-  minGrade: Number,
-  maxGrade: Number,
-  trait: String
+  grade: {
+    type: String,
+    required: true
+  },
+  trait: {
+    type: String,
+    required: true
+  }
 });
 
-const TraitRange = mongoose.model('TraitRange', traitRangeSchema);
-
-module.exports = TraitRange;
+module.exports = mongoose.model('TraitRange', traitRangeSchema);
