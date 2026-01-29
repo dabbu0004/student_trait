f4d373b42b0e7ed6ded88c68baf944637df86b3c
Himanshu <166318192+dabbu0004@users.noreply.github.com>
2026-01-29 13:57:16 +0530

    Modify GradeRange schema to require grade as string
    Updated grade field to be a required string in the schema.

--- backend/models/GradeRange.js
+++ backend/models/GradeRange.js
@@ -1,9 +1,25 @@
+// const mongoose = require('mongoose');
+
+// const gradeRangeSchema = new mongoose.Schema({
+//   minAvg: Number,
+//   maxAvg: Number,
+//   grade: Number
+// });
+
+// const GradeRange = mongoose.model('GradeRange', gradeRangeSchema);
+
+
+// module.exports = GradeRange;
 const mongoose = require('mongoose');
 
 const gradeRangeSchema = new mongoose.Schema({
   minAvg: Number,
   maxAvg: Number,
-  grade: Number
+  grade: {
+  type: String,
+  required: true
+}
+
 });
 
 const GradeRange = mongoose.model('GradeRange', gradeRangeSchema);
